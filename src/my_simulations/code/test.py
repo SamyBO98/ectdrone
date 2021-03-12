dcec1bb3dbd5ba1b58e5a4c17a9575ed8969c9da
samy <samy.ben-othman@etu.univ-lyon1.fr>
2021-03-12 14:22:23 +0100

    2 drones can now take off

--- src/my_simulations/code/test.py
+++ src/my_simulations/code/test.py
@@ -7,53 +7,41 @@ import math
 import argparse
 from pymavlink import mavutil
 
+connection_string1 = '127.0.0.1:14550'
+connection_string2 = '127.0.0.1:14560'
+vehicle1 = connect(connection_string1,wait_ready=True)
+vehicle2 = connect(connection_string2,wait_ready=True)
 
-def connectMyCopter():
-    parser = argparse.ArgumentParser(description='commands')
-    parser.add_argument('--connect')
-    args = parser.parse_args()
-
-    connection_string = args.connect
-
-    if not connection_string:
-        import dronekit_sitl
-        sitl = dronekit_sitl.start_default()
-        connection_string = sitl.connection_string()
-
-    vehicle = connect(connection_string,wait_ready=True)
-    return vehicle
-
-def arm_and_takeoff(aTargetAltitude):
+def arm_and_takeoff(vehicle,aTargetAltitude):
     """
     Arms vehicle and fly to aTargetAltitude.
     """
-    connected_drone = connectMyCopter()
 
     print "Basic pre-arm checks"
     # Don't try to arm until autopilot is ready
-    while not connected_drone.is_armable:
+    while not vehicle.is_armable:
         print " Waiting for vehicle to initialise..."
         time.sleep(1)
 
     print "Arming motors"
     # Copter should arm in GUIDED mode
-    connected_drone.mode    = VehicleMode("GUIDED")
-    connected_drone.armed   = True
+    vehicle.mode    = VehicleMode("GUIDED")
+    vehicle.armed   = True
 
     # Confirm vehicle armed before attempting to take off
-    while not connected_drone.armed:
+    while not vehicle.armed:
         print " Waiting for arming..."
         time.sleep(1)
 
     print "Taking off!"
-    connected_drone.simple_takeoff(aTargetAltitude) # Take off to target altitude
+    vehicle.simple_takeoff(aTargetAltitude) # Take off to target altitude
 
     # Wait until the vehicle reaches a safe height before processing the goto (otherwise the command
     #  after Vehicle.simple_takeoff will execute immediately).
     while True:
-        print " Altitude: ", connected_drone.location.global_relative_frame.alt
+        print " Altitude: ", vehicle.location.global_relative_frame.alt
         #Break and return from function just below target altitude.
-        if connected_drone.location.global_relative_frame.alt>=aTargetAltitude*0.95:
+        if vehicle.location.global_relative_frame.alt>=aTargetAltitude*0.95:
             print "Reached target altitude"
             break
         time.sleep(1)
@@ -61,4 +49,5 @@ def arm_and_takeoff(aTargetAltitude):
 
 
 if __name__ == "__main__":
-    arm_and_takeoff(20)
+    arm_and_takeoff(vehicle1,10)
+    arm_and_takeoff(vehicle2,20)
