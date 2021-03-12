19ef71c49aa0dffbf3f712ba7a7f788aade328d8
samy <samy.ben-othman@etu.univ-lyon1.fr>
2021-03-12 12:50:52 +0100

    Double drone display

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -131,6 +131,7 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("/home/samy/ectdrone/build/gtest/cmake_install.cmake")
+  include("/home/samy/ectdrone/build/my_simulations/cmake_install.cmake")
 
 endif()
 
--- build/my_simulations/CTestTestfile.cmake
+++ build/my_simulations/CTestTestfile.cmake
@@ -1,8 +1,6 @@
 # CMake generated Testfile for 
-# Source directory: /home/samy/ectdrone/src
-# Build directory: /home/samy/ectdrone/build
+# Source directory: /home/samy/ectdrone/src/my_simulations
+# Build directory: /home/samy/ectdrone/build/my_simulations
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("my_simulations")
